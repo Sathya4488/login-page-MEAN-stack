58d79c696b817587ec661917d7ef267ea7fd1054
Sathya4488 <48579570+Sathya4488@users.noreply.github.com>
2019-04-12 15:15:10 +0530

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
   'browserName': 'chrome',
   'chromeOptions': {
-     args: ["--headless"]
+     args: ["--headless", "--disable-gpu", "--window-size=800x600"]
   }
 },
 
