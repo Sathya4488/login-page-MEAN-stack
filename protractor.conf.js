db2c9df3760b2ff12c21c7b7da6730cfc06bbf4d
Sathya4488 <48579570+Sathya4488@users.noreply.github.com>
2019-04-23 18:51:29 +0530

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -10,6 +10,9 @@ exports.config = {
   ],
   capabilities: {
     'browserName': 'chrome'
+    chromeOptions: {
+            args: ["--headless", "--disable-gpu", "--window-size=800x600"]
+  }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
